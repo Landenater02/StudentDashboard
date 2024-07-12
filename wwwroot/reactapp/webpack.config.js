79b946bc13825dee21ff21bce5c248e95bde9821
Landen Hawley <lhawley@hbs.net>
2024-07-12 08:59:23 -0500

    Implementing react into project

---
+++ wwwroot/reactapp/webpack.config.js
@@ -0,0 +1,31 @@
+﻿const path = require('path');
+  module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    externals: {
+      react: 'React', 
+      'react-dom': 'ReactDOM', 
+    },
+  };
